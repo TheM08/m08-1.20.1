3c547d4b8a9e8ae51e6aedd5935146616974ed97
Ellis McKenzie <ellismck08@gmail.com>
2023-11-11 21:39:21 +0000

    Initial commit

--- src/main/java/net/m08/m08mod/M08Mod.java
+++ src/main/java/net/m08/m08mod/M08Mod.java
@@ -1,9 +1,9 @@
 package net.m08.m08mod;
 
 import net.m08.m08mod.item.ModItems;
-import net.m08.m08mod.item.ModItemsGroups;
 import net.fabricmc.api.ModInitializer;
 
+import net.m08.m08mod.item.ModItemsGroups;
 import net.m08.m08mod.util.ModCustomTrades;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,10 +14,8 @@ public class M08Mod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
 		ModItems.registerModItems();
 		ModItemsGroups.registerItemGroups();
-
 
 		ModCustomTrades.registerCustomTrades();
 	}

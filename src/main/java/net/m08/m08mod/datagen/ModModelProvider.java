6040da2b2ea54a8c05f9dea8b26dc4cca8444031
Ellis McKenzie <ellismck08@gmail.com>
2023-12-01 18:57:06 +0000

    Initial commit

--- src/main/java/net/m08/m08mod/datagen/ModModelProvider.java
+++ src/main/java/net/m08/m08mod/datagen/ModModelProvider.java
@@ -7,7 +7,6 @@ import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.minecraft.data.client.BlockStateModelGenerator;
 import net.minecraft.data.client.ItemModelGenerator;
 import net.minecraft.data.client.Models;
-import net.minecraft.item.Item;
 
 public class ModModelProvider extends FabricModelProvider {
     public ModModelProvider(FabricDataOutput output) {
@@ -16,7 +15,7 @@ public class ModModelProvider extends FabricModelProvider {
 
     @Override
     public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
-
+        blockStateModelGenerator.registerCubeAllModelTexturePool(ModBlocks.CREDIT_CHEST);
     }
 
     @Override
